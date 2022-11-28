a6d3d9e50937949735fc0eb68cf501c6bd2eea21
foolish-pine <matsuda.takuya41@gmail.com>
2022-11-28 13:58:12 +0900

    cp59

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,5 +3,16 @@ import { FC } from "react";
 import "components/Main.css";
 
 export const Main: FC = () => {
-	return <div>Main</div>;
+	return (
+		<div className="app-main">
+			<div className="app-main-note-edit">
+				<input type="text" />
+				<textarea id="" placeholder="ノート内容を記入"></textarea>
+			</div>
+			<div className="app-main-note-preview">
+				<h1 className="preview-title">タイトル</h1>
+				<div className="markdown-preview">ノート内容</div>
+			</div>
+		</div>
+	);
 };
