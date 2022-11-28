3c7bdcc8a1de6879cf67b9972756111724b23ded
foolish-pine <matsuda.takuya41@gmail.com>
2022-11-28 12:27:09 +0900

    cp52

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,5 +3,22 @@ import { FC } from "react";
 import "components/Sidebar.css";
 
 export const Sidebar: FC = () => {
-	return <div>Sidebar</div>;
+	return (
+		<aside className="app-sidebar">
+			<div className="app-sidebar-header">
+				<h1>ノート</h1>
+				<button>追加</button>
+			</div>
+			<div className="app-sidebar-notes">
+				<div className="app-sidebar-note">
+					<div className="sidebar-note-title">
+						<strong>タイトル</strong>
+						<button>削除</button>
+					</div>
+					<p>ノートの内容です。</p>
+					<small>最終更新日：2022/12/01</small>
+				</div>
+			</div>
+		</aside>
+	);
 };
